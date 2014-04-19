a6739fdd2028881ab0f3da5fc479f38cb1dea133
song860918 <song860918@gmail.com>
2014-04-18 23:07:18 -0700

    add edit functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,14 @@
 class RecipesController < ApplicationController
-  
+ 
+  def index
+    @recipes = Recipe.all
+  end
+ 
   def new
   end
 
   def create
-    @recipe = Recipe.new(params.require(:recipe).permit(:title, :text))
+    @recipe = Recipe.new(precipe_params)
     @recipe.save
     redirect_to @recipe
   end
@@ -12,5 +16,20 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :text)
+    end
 
 end
